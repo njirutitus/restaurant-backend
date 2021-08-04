a2b2b5d258e6a33dad18f6f378c9a6ad90fb28a9
titus njiru <njirutitus@gmail.com>
2021-08-04 15:54:45 +0300

    deleting the send_mail function'

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -118,49 +118,4 @@ class SiteController extends Controller
         return $this->render('dashboard');
     }
 
-
-    /**
-     * @return bool
-     */
-    public function send_mail(): bool
-    {
-        $mail = new PHPMailer(true);
-
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'royalkinginvest@gmail.com';                     //SMTP username
-            $mail->Password   = 'Xg!=c5uP$UGkZXHG';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-            //Recipients
-            $mail->setFrom('royalkinginvest@gmail.com', 'Royal King Investment');
-            $mail->addAddress('titokym96@gmail.com', 'Tito Kim');     //Add a recipient
-            $mail->addReplyTo('royalkinginvest@gmail.com', 'Royal King Investment');
-//            $mail->addCC('');
-//            $mail->addBCC('');
-
-            //Attachments
-//            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-//            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-            $mail->send();
-            return true;
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-            exit();
-            return false;
-        }
-    }
-
 }
