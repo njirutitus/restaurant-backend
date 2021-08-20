933e4d3e256753a7947402aec043a468b1d888fa
titus njiru <njirutitus@gmail.com>
2021-08-21 00:50:34 +0300

    add reservation form model

---
+++ models/ReservationForm.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace app\models;
+
+
+use tn\phpmvc\DbModel;
+
+class ReservationForm extends DbModel
+{
+    public string $reserved_by = '';
+    public string $date = '';
+    public string $time = '';
+    public string $adults = '';
+
+    public static function tableName(): string
+    {
+        return 'reservation';
+    }
+
+    public function attributes(): array
+    {
+        return array('reserved_by','date','time','adults');
+    }
+
+    public static function primaryKey(): string
+    {
+        return 'id';
+    }
+
+    public function rules(): array
+    {
+        return [
+            'reserved_by' => [self::RULE_REQUIRED],
+            'date' => [self::RULE_REQUIRED],
+            'time' => [self::RULE_REQUIRED],
+            'adults' => [self::RULE_REQUIRED,self::RULE_NUMBER],
+        ];
+    }
+
+    public function add() : bool
+    {
+        return parent::save();
+    }
+}
