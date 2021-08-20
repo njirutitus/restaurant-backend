09b8a43ecc6a6285bdc6743b1c4194cb4e955393
titus njiru <njirutitus@gmail.com>
2021-08-21 02:49:53 +0300

    configure env vars

--- manage.php
+++ manage.php
@@ -7,20 +7,33 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+
+if (!getenv('DB_DSN')) {
+    putenv("DB_DSN=$_ENV[DB_DSN]");
+    putenv("DB_USER=$_ENV[DB_USER]");
+    putenv("DB_PASSWORD=$_ENV[DB_PASSWORD]");
+    putenv("MAIL_HOST=$_ENV[MAIL_HOST]");
+    putenv("MAIL_USERNAME=$_ENV[MAIL_USERNAME]");
+    putenv("MAIL_PASSWORD=$_ENV[MAIL_PASSWORD]");
+    putenv("MAIL_PORT=$_ENV[MAIL_PORT]");
+
+}
+
 $config = [
     'userClass' => \app\models\User::class,
     'db' => [
-        'dsn' => $_ENV['DB_DSN'],
-        'user' => $_ENV['DB_USER'],
-        'password' => $_ENV['DB_PASSWORD']
+        'dsn' => getenv('DB_DSN'),
+        'user' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD')
     ],
     'mail' => [
-        'host' => $_ENV['MAIL_HOST'],
-        'username' => $_ENV['MAIL_USERNAME'],
-        'password' => $_ENV['MAIL_PASSWORD'],
-        'port' => $_ENV['MAIL_PORT'],
+        'host' => getenv('MAIL_HOST'),
+        'username' => getenv('MAIL_USERNAME'),
+        'password' => getenv('MAIL_PASSWORD'),
+        'port' => getenv('MAIL_PORT'),
     ]
 ];
+
 
 $app = new Application(__DIR__, $config);
 
