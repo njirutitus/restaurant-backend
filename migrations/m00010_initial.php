2e3b8ebd9e6d5526d4ddaf8834e355b461e124d1
titus njiru <njirutitus@gmail.com>
2021-08-21 03:23:51 +0300

    add is_staff column to user table

---
+++ migrations/m00010_initial.php
@@ -0,0 +1,18 @@
+<?php
+
+class m0007_initial
+{
+    public function up()
+    {
+        $db = \tn\phpmvc\Application::$app->db;
+        $SQL = "ALTER TABLE `user` ADD (is_staff BOOLEAN NOT NULL DEFAULT 0)" ;
+        $db->pdo->exec($SQL);
+    }
+
+    public function down()
+    {
+        $db = \tn\phpmvc\Application::$app->db;
+        $SQL = "ALTER TABLE `user` DROP IF EXISTS is_super_admin";
+        $db->pdo->exec($SQL);
+    }
+}
